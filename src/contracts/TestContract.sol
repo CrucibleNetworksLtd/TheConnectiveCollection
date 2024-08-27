b8104e9120407e34004394a8e5e9fbd10aa88026
pato <pato.sabogal@gmail.com>
2024-08-27 15:02:47 -0300

    Adds supply limit.

--- src/contracts/TestContract.sol
+++ src/contracts/TestContract.sol
@@ -11,13 +11,14 @@ contract TestContract is ERC721URIStorage, Ownable {
     address public treasuryWallet;
     bytes32 public freeMerkleRoot;
     bytes32 public paidMerkleRoot;
-    uint256 public mintingState = 0;
+    uint8 public mintingState = 0;
     uint256 public whitelistMintPrice = 20000000000000000; // 0.02 ether in wei
     uint256 public mintPrice = 30000000000000000; // 0.03 ether in wei
+    uint16 private _maxSupply = 2500;
     string private baseTokenURI;
     bool public mintingIsActive = false;
     mapping(address => bool) public hasMintedFree;
-    uint256 private _tokenIdCounter = 0;
+    uint16 private _tokenIdCounter = 0;
 
     constructor(string memory name, string memory symbol) Ownable(msg.sender) ERC721(name, symbol) {}
 
@@ -26,7 +27,7 @@ contract TestContract is ERC721URIStorage, Ownable {
         treasuryWallet = _treasuryWallet;
     }
 
-    function setMintingState(uint256 _state) external onlyOwner {
+    function setMintingState(uint8 _state) external onlyOwner {
         require(_state >= 0 && _state < 3, "Invalid minting state");
         mintingState = _state;
     }
@@ -55,9 +56,11 @@ contract TestContract is ERC721URIStorage, Ownable {
         mintingIsActive = !mintingIsActive;
     }
 
-    function mint(uint256 amount, bytes32[] calldata merkleProof) external payable {
+    function mint(uint16 amount, bytes32[] calldata merkleProof) external payable {
         require(mintingIsActive, "Minting is not active");
         require(amount > 0, "Must mint at least one token");
+        require(_tokenIdCounter < _maxSupply, "Minting maximum reached.");
+        require(_tokenIdCounter + amount <= _maxSupply, "Minting amount exceeds maximum supply.");
 
         bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
 
@@ -73,8 +76,8 @@ contract TestContract is ERC721URIStorage, Ownable {
             require(msg.value == mintPrice * amount, "Insufficient funds for public minting");
         }
 
-        for (uint256 i = 0; i < amount; i++) {
-            uint256 tokenId = _tokenIdCounter;
+        for (uint16 i = 0; i < amount; i++) {
+            uint16 tokenId = _tokenIdCounter;
             _mint(msg.sender, tokenId);
             _tokenIdCounter++;
         }
@@ -84,7 +87,7 @@ contract TestContract is ERC721URIStorage, Ownable {
         require(treasuryWallet != address(0), "Treasury not set.");
         uint256 balance = address(this).balance;
         require(balance > 0, "No funds to withdraw");
-        payable(treasuryWallet).transfer(balance); // add treasury wallet
+        payable(treasuryWallet).transfer(balance);
     }
 
     function totalSupply() public view returns (uint256) {
